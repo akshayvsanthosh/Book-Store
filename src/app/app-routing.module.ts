9759491b16ed981606a5f9e2bac366bc98d9fec2
akshayvsanthosh <akshaysanthosh12345@gmail.com>
2024-10-03 12:17:32 +0530

    view books page added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ViewBooksComponent } from './view-books/view-books.component';
 
 const routes: Routes = [
-  {path:"home",component:HomeComponent}
+  {path:"home",component:HomeComponent},
+  {path:"books",component:ViewBooksComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { BooksComponent } from './books/books.component';
 import { FooterComponent } from './footer/footer.component';
+import { ViewBooksComponent } from './view-books/view-books.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { FooterComponent } from './footer/footer.component';
     HeaderComponent,
     HomeComponent,
     BooksComponent,
-    FooterComponent
+    FooterComponent,
+    ViewBooksComponent
   ],
   imports: [
     BrowserModule,
